8ec4e1c056aed02d3069236ab2c147a3ad68f591
LGWH_LC <contact@lgwh.work>
2018-07-07 23:59:22 -0400

    Accepts user input. Still needs user input validation. LAB INCOMPLETE.

--- src/grandcircus_javabootcamp/DatesCalc.java
+++ src/grandcircus_javabootcamp/DatesCalc.java
@@ -1,10 +1,9 @@
 package grandcircus_javabootcamp;
 
-// Chose newer java.time.* from SE8 over java.util.Calendar for ease of use.
 import java.time.LocalDate;
-import java.time.Month;
 import java.time.Period;
-// import java.util.Scanner; to be implemented.
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 /*
  * Write a Java console application to calculate the duration between two dates
@@ -16,15 +15,33 @@ import java.time.Period;
 public class DatesCalc {
 	public static void main(String[] args) {
 
-		// TODO implement user inputed dates.
+		// LocalDate dateOne = LocalDate.of(1900, Month.JANUARY, 1);
+		// LocalDate dateTwo = LocalDate.of(2000, Month.JANUARY, 1);
+		String strFirstDate = "1900/01/01";
+		String strSecondDate = "2000/01/01";
 
-		LocalDate dateOne = LocalDate.of(1978, Month.OCTOBER, 3);
-		LocalDate dateTwo = LocalDate.of(2018, Month.JULY, 7);
+		// Create scanner object.
+		Scanner input = new Scanner(System.in);
 
-		System.out.println("Calculating between dates of " + dateOne + " and " + dateTwo + "...");
+		// Prompt user for the first date to compare.
+		System.out.println("Enter the first date in Year/Month/Day format: [Example: 1938/10/31] :");
+		strFirstDate = input.nextLine(); // Reads user input as a string.
+		DateTimeFormatter formatDate = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+		LocalDate firstDate = LocalDate.parse(strFirstDate, formatDate);
 
-		Period period = Period.between(dateOne, dateTwo);
+		// Prompt user for the first date to compare.
+		System.out.println("Enter the first date in Year/Month/Day format: [Example: 1938/10/31] :");
+		strSecondDate = input.nextLine(); // Reads user input as a second string.
+		DateTimeFormatter formatDate2 = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+		LocalDate secondDate = LocalDate.parse(strSecondDate, formatDate2);
 
+		// Feedback as to what the program is doing...
+		System.out.println("Calculating between dates of " + firstDate + " and " + secondDate + "...");
+
+		// Compares two properly formated dates using the java.time.Period method.
+		Period period = Period.between(firstDate, secondDate);
+
+		// Displays result...
 		System.out.println("That is a difference of " + period.getYears() + " years, " + period.getMonths()
 				+ " months, and " + period.getDays() + " days.");
 	}
